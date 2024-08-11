28874119cdaa9bc01321e9a611974062430c71d6
jimchoi <jimchoi@student.42seoul.kr>
2024-08-11 20:30:33 +0900

    ex02

---
+++ cpp_module_00/ex02/Account.cpp
@@ -0,0 +1,65 @@
+#include "Account.hpp"
+
+int Account::getNbAccounts(void)
+{
+
+}
+
+int Account::getTotalAmount(void)
+{
+
+}
+
+int Account::getNbDeposits(void)
+{
+
+}
+
+int Account::getNbWithdrawals(void)
+{
+
+}
+void Account::displayAccountsInfos(void)
+{
+
+}
+
+void Account::makeDeposit(int deposit)
+{
+
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int Account::checkAmount(void) const
+{
+
+}
+void Account::displayStatus(void) const
+{
+
+}
+
+void Account::_displayTimestamp(void)
+{
+
+}
+
+
+Account::Account(int initail_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+
+}
+
+Account::Account(void)
+{
+
+}
